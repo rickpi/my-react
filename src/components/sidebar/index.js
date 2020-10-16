2664e363ffebaf97c5dd41243f1f72350d44e570
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-16 11:31:53 +0200

    feat(adBanner): add an ad banner on the sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { Image } from 'react-bootstrap';
+
 import SignIn from '../signin';
 
 const Sidebar = () => (
   <div>
     <SignIn />
     <hr />
+    <Image className="mb-4" src="https://infiniteingenuity.files.wordpress.com/2015/03/skyscraper.jpg" fluid />
   </div>
 );
 
