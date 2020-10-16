412bd59c84df7ec49a12cbd2718607b3d784f354
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-16 14:09:47 +0200

    chore(Navbar): put off the navbar of the row

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import {
   Container,
-  Row,
-  Col,
   Navbar,
   Nav,
   Form,
@@ -12,26 +10,22 @@ import {
 } from 'react-bootstrap';
 
 const Navigation = () => (
-  <Row>
-    <Col>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">MyMovies</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Action</Nav.Link>
-            <Nav.Link href="#features">Aventure</Nav.Link>
-            <Nav.Link href="#pricing">Horreur</Nav.Link>
-            <Nav.Link href="#pricing">Comédie</Nav.Link>
-            <Nav.Link href="#pricing">Sign-in</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Container>
-      </Navbar>
-    </Col>
-  </Row>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">MyMovies</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Action</Nav.Link>
+        <Nav.Link href="#features">Aventure</Nav.Link>
+        <Nav.Link href="#pricing">Horreur</Nav.Link>
+        <Nav.Link href="#pricing">Comédie</Nav.Link>
+        <Nav.Link href="#pricing">Sign-in</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Container>
+  </Navbar>
 );
 
 export default Navigation;
