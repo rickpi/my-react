413346b7defb03863148bd9c5bad4caa1873ad16
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-16 10:59:52 +0200

    style(Trailers): add margin

--- src/components/trailers/index.js
+++ src/components/trailers/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Trailers = () => (
   <div>
-    <h4>Bandes Annonces</h4>
+    <h4 className="mt-3">Bandes Annonces</h4>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static.hitek.fr/img/up_m/362459579/ob3a7b42thehobbit2.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/04/Avengers-Endgame.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   </div>
 );
 
