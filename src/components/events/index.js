62a0be79c824b325ab3b013b9c22dde06b45e6c5
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-18 15:21:38 +0200

    chore(events): put events's datas on a separate file

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -10,55 +10,7 @@ import { Cash, Building } from 'react-bootstrap-icons';
 import Filters from '../filters';
 import Tags from '../tags';
 
-const events = [{
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille;C\'est un peu;A chier;On va pas;Se mentir',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}, {
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}, {
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}, {
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}, {
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}, {
-  title: 'ELLE PART DANS DES TRUCS',
-  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
-  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
-  tags: 'Ados;En famille;C\'est un peu;A chier;On va pas;Se mentir',
-  priceType: 'payant',
-  addressCity: 'Paris',
-  id: '109066',
-}];
+import events from '../../datas/events';
 
 const EventItem = ({ data }) => {
   const {
---
+++ src/datas/events.js
@@ -0,0 +1,51 @@
+const events = [{
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille;C\'est un peu;A chier;On va pas;Se mentir',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}, {
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}, {
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}, {
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}, {
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}, {
+  title: 'ELLE PART DANS DES TRUCS',
+  leadText: 'Claire-Marie se raconte, se disperse et s\'égare pour notre plus grand bonheur. Un spectacle qui fait du bien !',
+  coverUrl: 'https://cdn.paris.fr/qfap/2020/08/18/72516_VmlzdWVsIHBhcmlzZnIgZWxsZSBwYXJ0IGRhbnMgZGVzIHRydWNzLmpwZw==.jpg',
+  tags: 'Ados;En famille;C\'est un peu;A chier;On va pas;Se mentir',
+  priceType: 'payant',
+  addressCity: 'Paris',
+  id: '109066',
+}];
+
+export default events;
