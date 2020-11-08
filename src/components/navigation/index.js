eaf000fd84135958a4c207c4f88b176721de3a80
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-11-08 19:12:26 +0100

    update(navigation): transform navigation component into component with state

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Globe2 } from 'react-bootstrap-icons';
 import {
   Navbar,
@@ -9,27 +9,40 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const Navigation = () => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand>
-      <Nav.Link href="/">
-        <Globe2 className="mr-2" />
-        {'{ ReactApp }'}
-      </Nav.Link>
-    </Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Evénement</Nav.Link>
-      <Nav.Link href="/about">A propos</Nav.Link>
-    </Nav>
-    <Form inline>
-      <InputGroup size="sm">
-        <FormControl type="text" placeholder="Rechercher un événement..." className="mr-sm-2" />
-        <InputGroup.Append>
-          <Button variant="outline-secondary">Lancer la recherche</Button>
-        </InputGroup.Append>
-      </InputGroup>
-    </Form>
-  </Navbar>
-);
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: props.inputValue,
+    };
+  }
+
+  render() {
+    const { inputValue } = this.state;
+
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <Nav.Link href="/">
+            <Globe2 className="mr-2" />
+            {'{ ReactApp }'}
+          </Nav.Link>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Evénement</Nav.Link>
+          <Nav.Link href="/about">A propos</Nav.Link>
+        </Nav>
+        <Form inline>
+          <InputGroup size="sm">
+            <FormControl type="text" placeholder="Rechercher un événement..." className="mr-sm-2" value={inputValue} />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">Lancer la recherche</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
+      </Navbar>
+    );
+  }
+}
 
 export default Navigation;
