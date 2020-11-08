529ad0eaf304ad2ba0834c125e4cd6612689f956
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-11-08 17:06:13 +0100

    add(tagsList): add tags list constant

---
+++ src/constants/tagsList.js
@@ -0,0 +1,24 @@
+const tagsList = [
+  'Ados',
+  'Bibliothèques',
+  'Cinéma',
+  'En famille',
+  'Enfants',
+  'English',
+  'Étudiants',
+  'Expos',
+  'Geek',
+  'Gourmand',
+  'Insolite',
+  'Les Nuits',
+  'Musique',
+  'Noël',
+  'Plein Air',
+  'Queer Lgbt',
+  'Solidaire',
+  'Sport',
+  'Urbain',
+  'Végétalisons Paris',
+];
+
+export default tagsList;
