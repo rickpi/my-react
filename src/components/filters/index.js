e6443495c868be28f1d3ca3074aaf62395876240
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-11-08 17:33:11 +0100

    add(linkedTags): add linked tags in filters component

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Col,
+} from 'react-bootstrap';
 
-const Filters = () => <div>Filters</div>;
+import LinkedTags from '../linkedTags';
+
+import tagsList from '../../constants/tagsList';
+
+class Filters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: tagsList,
+      tagsSelected: [],
+    };
+  }
+
+  render() {
+    const { tags, tagsSelected } = this.state;
+
+    return (
+      <Col xs="3">
+        <h3 className="mt-4">Mots clés</h3>
+        <LinkedTags tags={tags.join(';')} />
+        <hr />
+        <h3 className="mt-4">Filtres</h3>
+        <LinkedTags tags={tagsSelected.join(';')} />
+      </Col>
+    );
+  }
+}
 
 export default Filters;
