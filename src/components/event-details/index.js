9fe19c932aa0c304dd2194d385d49b0a45412f2c
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-11-14 19:55:12 +0100

    style(map): style the map from google map api

--- src/components/event-details/index.js
+++ src/components/event-details/index.js
@@ -17,6 +17,7 @@ import Tags from '../tags';
 import './event-details.css';
 
 import url from '../../constants/url';
+import API_KEY from '../../constants/API_KEY';
 
 const ExportButton = ({ variant, href, value }) => (
   <Button size="sm" variant={variant} href={href} className="mr-1">
@@ -100,43 +101,56 @@ class EventDetails extends Component {
           <p>{Parser(about.description)}</p>
         </Col>
         <Col xs="1">{' '}</Col>
-        <Col className="event-detail__right-block" xs="4">
-          <h5 className="mb-3">A propos</h5>
-          <Col xs="12" className="mb-3 event-detail__price">
-            <Cash className="mr-3" />
-            <span>{about.price}</span>
+        <Col xs="4">
+          <Col className="event-detail__right-block" xs="12">
+            <h5 className="mb-3">A propos</h5>
+            <Col xs="12" className="mb-3 event-detail__price">
+              <Cash className="mr-3" />
+              <span>{about.price}</span>
+            </Col>
+            <Col xs="12" className="mb-3 event-detail__date">
+              <CalendarDate className="mr-3" />
+              <div>
+                {Parser(about.date)}
+              </div>
+            </Col>
+            <h5 className="mb-3 mt-4">Localisation</h5>
+            <Col xs="12" className="mb-3">
+              {Parser(`\
+                ${address.name}\
+                <br/>\
+                <strong>${address.city}</strong>\
+                <br/>\
+                ${address.zipcode}, ${address.street}\
+              `)}
+            </Col>
+            <h5 className="mb-3 mt-4">Contact</h5>
+            <Col xs="12">{contact.name}</Col>
+            <Col xs="12" className="mb-3">
+              <Envelope className="mr-2" />
+              {Parser(`<strong>${contact.mail}</strong>`)}
+            </Col>
+            <Col xs="12" className="mb-2">
+              <Row>
+                <ExportButton variant="primary" href={contact.facebook} value="FACEBOOK" />
+                <ExportButton variant="info" href={contact.twitter} value="TWITTER" />
+                <ExportButton variant="secondary" href={contact.url} value="SITE" />
+              </Row>
+            </Col>
+            <Col xs="12">
+              <Tags tags={about.tags} />
+            </Col>
           </Col>
-          <Col xs="12" className="mb-3 event-detail__date">
-            <CalendarDate className="mr-3" />
-            <div>
-              {Parser(about.date)}
-            </div>
-          </Col>
-          <h5 className="mb-3 mt-4">Localisation</h5>
-          <Col xs="12" className="mb-3">
-            {Parser(`\
-              ${address.name}\
-              <br/>\
-              <strong>${address.city}</strong>\
-              <br/>\
-              ${address.zipcode}, ${address.street}\
-            `)}
-          </Col>
-          <h5 className="mb-3 mt-4">Contact</h5>
-          <Col xs="12">{contact.name}</Col>
-          <Col xs="12" className="mb-3">
-            <Envelope className="mr-2" />
-            {Parser(`<strong>${contact.mail}</strong>`)}
-          </Col>
-          <Col xs="12" className="mb-2">
-            <Row>
-              <ExportButton variant="primary" href={contact.facebook} value="FACEBOOK" />
-              <ExportButton variant="info" href={contact.twitter} value="TWITTER" />
-              <ExportButton variant="secondary" href={contact.url} value="SITE" />
-            </Row>
-          </Col>
-          <Col xs="12">
-            <Tags tags={about.tags} />
+          <Col className="event-detail__map" xs="12">
+            <iframe
+              frameBorder="0"
+              src={`https://www.google.com/maps/embed/v1/place?\
+                key=${API_KEY}&\
+                q=${encodeURI(`${address.street},${address.city} France`)}\
+              `.replace(/ /g, '')}
+              title="location"
+              allowFullScreen
+            />
           </Col>
         </Col>
       </Row>
